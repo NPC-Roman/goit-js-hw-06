1d7faf3faf6f4338aa6a2611204577b9b45d36ee
NPC-Roman <ispacebobo@gmail.com>
2023-07-29 18:08:05 +0300

    Ch2

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,24 @@
-const categoriesNum = document.querySelector('#categories');
-console.log(categoriesNum);
-const items =  document.querySelectorAll('.item');
-console.log(items);
+/*const categoriesList = document.getElementById("categories");
+
+// Отримуємо всі пункти категорій з класом "item"
+const categoryItems = categoriesList.querySelectorAll(".item");
+
+// Пораховуємо кількість категорій (пунктів з класом "item")
+const numberOfCategories = categoryItems.length;
+console.log(`Number of categories: ${numberOfCategories}`);
+
+// Проходимося по кожному пункті категорії та виводимо інформацію в консоль
+categoryItems.forEach((item) => {
+  // Отримуємо заголовок (h2) з кожного пункту категорії
+  const categoryTitle = item.querySelector("h2").textContent;
+
+  // Отримуємо всі елементи (li) вкладеного списку кожної категорії
+  const elementsInCategory = item.querySelectorAll("ul li");
+
+  // Пораховуємо кількість елементів у категорії
+  const numberOfElements = elementsInCategory.length;
+
+  // Виводимо інформацію про категорію та кількість її елементів в консоль
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${numberOfElements}`);
+});*/
